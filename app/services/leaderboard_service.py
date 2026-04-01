f62c1baf9fdfb32c0b3b4f2b5afc0c04b8f89228
katGhost <katghost124@gmail.com>
2026-04-01 16:03:37 +0200

    refactor: leaderboard service

--- app/services/leaderboard_service.py
+++ app/services/leaderboard_service.py
@@ -1,6 +1,22 @@
-def calculate_points(commits):
-    return commits  # simple for MVP
+
+from app.services.github_service import GithubService
+# def calculate_points(commits):
+#     return commits  # simple for MVP
 
 
-def rank_users(users):
-    return sorted(users, key=lambda x: x["points"], reverse=True)
+# def rank_users(users):
+#     return sorted(users, key=lambda x: x["points"], reverse=True)
+
+def get_leaderboard(users):
+    """
+    users: list of dicts {'id': ..., 'github_token': ...}
+    """
+    leaderboard = []
+    for user in users:
+        github = GithubService(user["github_token"])
+        score = github.get_weekly_contributions()
+        leaderboard.append({"id": user["id"], "score": score})
+
+    # Sort descending by score
+    leaderboard.sort(key=lambda x: x["score"], reverse=True)
+    return leaderboard
