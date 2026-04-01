caaa9d07400f6f7cc793a0187b8c4d26a0da9275
katGhost <katghost124@gmail.com>
2026-04-01 16:00:45 +0200

    Configure SECRET_KEY

--- app/config.py
+++ app/config.py
@@ -1,13 +1,18 @@
 import os
 from dotenv import load_dotenv
 
+os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'
 load_dotenv()
 
 class Config:
-    SECRET_KEY = os.getenv("SECRET_KEY", "developer-secret-key")
+    SECRET_KEY = os.urandom(24)
 
+    # OAuth
     AUTH0_CLIENT_ID = os.getenv("AUTH0_CLIENT_ID")
     AUTH0_CLIENT_SECRET = os.getenv("AUTH0_CLIENT_SECRET")
     AUTH0_DOMAIN = os.getenv("AUTH0_DOMAIN")
 
-    GITHUB_API = "https://api.github.com"
+    # GitHub
+    GITHUB_CLIENT_ID = os.getenv("GITHUB_CLIENT_ID")
+    GITHUB_CLIENT_SECRET = os.getenv("GITHUB_CLIENT_SECRET")
+    GITHUB_API_BASE_URL = "https://api.github.com"
