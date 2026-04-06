d490baf17a718c9a57caa2c26e4497083bde3e51
katGhost <katghost124@gmail.com>
2026-04-06 07:01:44 +0200

    fix: tenant issue resolved -> token vault success

--- app/oauth.py
+++ app/oauth.py
@@ -1,5 +1,4 @@
 from authlib.integrations.flask_client import OAuth
-from app.config import Config
 
 oauth = OAuth()
 
@@ -8,11 +7,11 @@ def init_oauth(app):
 
     oauth.register(
         name='auth0',
-        client_id=Config.AUTH0_CLIENT_ID,
-        client_secret=Config.AUTH0_CLIENT_SECRET,
-        server_metadata_url=f'https://{Config.AUTH0_DOMAIN}/.well-known/openid-configuration',
+        client_id=app.config['AUTH0_CLIENT_ID'],
+        client_secret=app.config['AUTH0_CLIENT_SECRET'],
+        server_metadata_url=f'https://{app.config['AUTH0_DOMAIN']}/.well-known/openid-configuration',
         client_kwargs={
-            'scope': 'openid profile email'
+            'scope': 'openid profile email offline_access'
         },
     )
 
